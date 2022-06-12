4e10fd87c7f08eb507e31d5572c40789afa043de
Camilo Lopez <jclopez.9708@gmail.com>
2022-06-12 15:48:42 -0500

    first commit

---
+++ src/types/types.js
@@ -0,0 +1,28 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event] Logout event ',
+
+    eventStartAddNew: '[event] Start add new',
+
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Events loaded',
+
+    authChecking: '[auth] checking login state',
+    authCheckingFinish: '[auth] finish checking login state',
+
+    authStartLogin: '[auth] start login',
+    authLogin: '[auth] Login',
+
+    authStartRegister: '[auth] Start Register',
+    authStartStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] logout',
+
+}
